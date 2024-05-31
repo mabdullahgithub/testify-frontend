08635ebad48f979dac8a06ac4ec88577d341911d
riz-mansha <exportinglobe@gmail.com>
2024-05-31 15:27:50 -0700

    remove some of extra file and pages.

--- src/app/(dashboard)/page.jsx
+++ src/app/(dashboard)/page.jsx
@@ -2,7 +2,7 @@ import DefaultLayout from "../../components/Layouts/DefaultLayout";
 import ECommerce from "../../components/Dashboard/E-commerce";
 
 export const metadata = {
-  title: "Arozeen Store",
+  title: "Rocco Store",
   description: "This is Rocco Ecommerce Website",
 };
 
--- src/app/config/actions.js
+++ src/app/config/actions.js
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 import { toast } from "react-toastify";
 
 export default async function LoginVendor(data) {
-  console.log("data:", data)
+  console.log("data:", data);
   try {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_BASE_URL}login`,
@@ -14,15 +14,6 @@ export default async function LoginVendor(data) {
 
     const expiryTime = new Date(Date.now() + 60 * 60 * 1000); // 1 hour from now
     if (response.data.status == "success") {
-    
-      cookies.set({
-        name: "id",
-        value: response.data.user.id,
-        expires: expiryTime,
-        httpOnly: true,
-        sameSite: "Lax", // Or 'Strict' for enhanced security
-      })
-      
       cookies().set({
         name: "token",
         value: response.data.token,
@@ -30,13 +21,12 @@ export default async function LoginVendor(data) {
         httpOnly: true,
         sameSite: "Lax", // Or 'Strict' for enhanced security
       });
-      
 
       console.log(response.data);
       return response.data;
     } else {
       console.log("error");
-      toast.error(response.data.message)
+      toast.error(response.data.message);
       return response.data.message;
     }
   } catch (error) {
@@ -59,17 +49,12 @@ export async function LogoutVendor() {
     },
   );
   console.log(response.data);
-  cookies().delete("id");
   cookies().delete("token");
   console.log("logout res:", response);
   return response.data;
 }
 
-
-
-export async function getToken(){
+export async function getToken() {
   const token = cookies().get("token").value;
-  
   return token;
-
 }
